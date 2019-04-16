41a0dd1cbc25423fc95176c41b75e13abc72c57a
Aqibjamshed <39996601+Aqibjamshed@users.noreply.github.com>
2019-04-16 12:06:18 +0500

    Update Task1.java

--- OOP-Lab1/Task1.java
+++ OOP-Lab1/Task1.java
@@ -6,9 +6,9 @@ float percent;
 char grade;
 boolean status;
 
-name = "Raja";
-marks = 486;
-percent = marks*100 / 550f;
+name = "Aqib";
+marks = 786;
+percent = marks*100 / 1100f;
 
 if(percent >= 80){
 grade = 'A';
@@ -27,7 +27,7 @@ status = false;
 }
 
 System.out.println("Name		  	=  "+name);
-System.out.println("Marks out of 550	=  "+marks);
+System.out.println("Marks out of 1100	=  "+marks);
 System.out.println("Percentage		=  "+percent);
 System.out.println("Grade			=  "+grade);
 if(status == true){
